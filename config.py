00855d11cb2fe248bdaed4b5162f82f4f206d117
kaixindelele <1985790413@qq.com>
2024-04-05 20:20:19 +0000

    readme fy

--- config.py
+++ config.py
@@ -84,7 +84,8 @@ DEFAULT_FN_GROUPS = ['学术']
 # 模型选择是 (注意: LLM_MODEL是默认选中的模型, 它*必须*被包含在AVAIL_LLM_MODELS列表中 )
 LLM_MODEL = "gpt-3.5-turbo" # 可选 ↓↓↓
 LLM_MODEL = "Qwen1.5-14B-Chat-GPTQ-Int4" # 可选 ↓↓↓
-AVAIL_LLM_MODELS = ["gpt-3.5-turbo", 'gpt-3.5-turbo-0301', 'gpt-3.5-turbo-0613', 
+AVAIL_LLM_MODELS = ["command-r-plus",
+                    "gpt-3.5-turbo", 'gpt-3.5-turbo-0301', 'gpt-3.5-turbo-0613',                     
                     "gpt-3.5-turbo-1106", "gpt-3.5-turbo-0125", "gpt-3.5-turbo-16k", 
                     "gpt-3.5-turbo-16k-0613", "Qwen1.5-14B-Chat-GPTQ-Int4"]
 # P.S. 其他可用的模型还包括 ["qianfan", "llama2", "qwen", "gpt-3.5-turbo-0613", "gpt-3.5-turbo-16k-0613",  "gpt-3.5-random"
